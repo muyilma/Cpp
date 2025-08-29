f079ce4f08242f4ec5de220d51439c02785330a1
Mustafa Emre Yılmaz <mustafaemreyilmz@gmail.com>
2025-08-29 15:12:58 +0300

    ex01 form finish

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,7 @@
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
 #include <cstdlib>
+#include <iomanip>
 
 Contact *PhoneBook::getUser() { return user; }
 void Contact::setName(std::string ipt) { name = ipt; }
@@ -29,6 +30,23 @@ int error_handle_long(std::string nbr)
     return 1;
 }
 
+std::string print(std::string str)
+{
+    std::string newstr="";
+    if (str.empty())
+        return "";
+    if (str.size() <= 10)
+        return str;
+    for (int i = 0; i < 10; i++)
+    {
+        if (i==9)
+            newstr+='.';
+        else
+            newstr+=str[i];
+    }
+    return newstr;
+}
+
 int error_handle_search(std::string nbr)
 {
     for (int i = 0; nbr[i]; i++)
@@ -40,11 +58,41 @@ int error_handle_search(std::string nbr)
     }
     return 1;
 }
+void search_display(Contact user[8])
+{
+    clear_screen();
+    std::cout << std::setfill('-') << std::setw(46) << "\n";
+    std::cout << "|" << "     " << "index" << "|" << " FirstName" << "|" << "   Surname" << "|" << "  Nickname" << "|" << std::endl;
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << std::setfill('-') << std::setw(46) << "\n";
+        std::cout << "|" << std::setfill(' ') << std::setw(10) << i+1 << "|" << std::setw(10) << print(user[i].getName()) << "|" << std::setw(10) << print(user[i].getSurName() )<< "|" << std::setw(10) << print(user[i].getNickName()) << "|" << std::endl;
+    }
+    std::cout << std::setfill('-') << std::setw(46) << "\n";
+    std::cout << "|"<< std::setfill(' ')  << std::setw(43) << "Write 9 to return" << "|" << std::endl;
+    std::cout << std::setfill('-') << std::setw(46) << "\n";
+}
+
+int user_check(Contact user)
+{
+    if (!(user.getName().empty()))
+        return 0;
+    if (!(user.getNickName().empty()))
+        return 0;
+    if (!(user.getSurName().empty()))
+        return 0;
+    if (!(user.getSecret().empty()))
+        return 0;
+    if (!(user.getPhoneNumber().empty()))
+        return 0;
+    return 1;
+}
 void PhoneBook::SearchUser(Contact user[8])
 {
     std::string number = "-1";
     int index=-1;
 
+    search_display(user);
     while (number != "9")
     {
         std::cout << "Search İndex:";
@@ -55,6 +103,10 @@ void PhoneBook::SearchUser(Contact user[8])
         index=atoi(number.c_str()) - 1;    
         if (number == "9")
             break;
+        if (user_check(user[index]))
+        {
+            continue;
+        }
         std::cout << "First Name:" << user[index].getName() << std::endl;
         std::cout << "Surname:" << user[index].getSurName() << std::endl;
         std::cout << "Nickname:" << user[index].getNickName() << std::endl;
@@ -100,6 +152,14 @@ void PhoneBook::UserAdd(Contact user[8], int *i)
         *i = 0;
 }
 
+void selection_screan()
+{
+    std::cout << std::setfill('-') << std::setw(25) << "\n";
+    std::cout << "| ADD | SEARCH | EXIT  |\n";
+    std::cout << std::setfill('-') << std::setw(25) << "\n";
+    std::cout << "İnput:";
+}
+
 int main()
 {
     PhoneBook cont;
@@ -108,7 +168,7 @@ int main()
 
     while (1)
     {
-        std::cout << "ADD    SEARCH     EXIT\n";
+        selection_screan();
         if (!std::getline(std::cin, name))
             break;
         if (name == "ADD")
