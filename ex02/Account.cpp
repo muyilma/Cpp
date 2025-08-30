be491fb8416c43300464d7665c1626b84d5e4251
Mustafa Emre Yılmaz <mustafaemreyilmz@gmail.com>
2025-08-30 12:55:28 +0300

    ex02 half done

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -8,14 +8,66 @@ int Account::_totalNbWithdrawals=0;
 
 void Account::displayAccountsInfos()
 {
-    std::cout << "  a";
+    std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount  << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 Account::Account(int initial_deposit)
 {
-    std::cout << "  a";
+    _totalAmount+=initial_deposit;
+    _amount=initial_deposit;
+    _accountIndex=_nbAccounts;
+    _nbDeposits=0;
+    _nbWithdrawals=0;
+    std::cout << "index:" << _accountIndex << ";" 
+              << "amount:" << _amount << ";created\n" ;//amount hesabındaki parayı gösteriyor
+    _nbAccounts++;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+
+    std::cout << "index:" << _accountIndex
+        << ";p_amount:" << _amount ;
+    if (_amount<withdrawal)
+        std::cout << ";withdrawal:refused\n";
+    else
+    {
+        _amount -= withdrawal;
+        _totalAmount -=withdrawal;
+        _nbWithdrawals++;
+        _totalNbWithdrawals++;
+        std::cout << ";withdrawal:" << withdrawal 
+                << ";amount:" << _amount
+                << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+    }
+
+    return _amount;
+}
+
+void Account::displayStatus() const
+{
+    std::cout << "index:" << _accountIndex
+            << ";amount:" << _amount 
+            << ";deposits:" << _nbDeposits 
+            << ";withdrawals:" << _nbWithdrawals << std::endl;
+
+}
+
+void Account::makeDeposit(int deposit)
+{
+    _nbDeposits++;
+    _totalNbDeposits++;
+    std::cout << "index:" << _accountIndex
+        << ";p_amount:" << _amount 
+        << ";deposit:" << deposit 
+        << ";amount:" << _amount + deposit
+        << ";nb_deposits:" << _nbDeposits << "\n";
+    _totalAmount +=deposit;
+    _amount +=deposit;
 }
 
 Account::~Account()
 {
-    std::cout << "  a";
+    std::cout << "index:" << _accountIndex << ";" 
+              << "amount:" << _amount << ";closed\n" ;//amount hesabındaki parayı gösteriyor
+    _nbAccounts++;
 }
--- ex02/Account.hpp
+++ ex02/Account.hpp
@@ -37,7 +37,7 @@ public:
 	int		checkAmount( void ) const;
 	void	displayStatus( void ) const;
 
-
+ 
 private:
 
 	static int	_nbAccounts;
