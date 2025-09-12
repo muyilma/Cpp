0eb0cdd195131580f79bb159ff2ff1dfa14e2f2b
Mustafa Emre Yılmaz <mustafaemreyilmz@gmail.com>
2025-09-12 17:19:03 +0300

    ex02 started

---
+++ Cpp02/ex02/fixed.hpp
@@ -0,0 +1,42 @@
+#ifndef FIXED
+#define FIXED
+#include <iostream>
+class Fixed
+{
+    private:
+        int number; 
+        static const int bitNumber=8; 
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(const Fixed &obj);//const koymadım referans değilde pointer yapabilir miydim?
+        Fixed(const int number);
+        Fixed(const float number);
+        
+        float toFloat( void ) const;
+        int toInt( void ) const;
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+        Fixed& operator=(const Fixed& copy);  
+        Fixed& operator>(const Fixed& copy);  
+        Fixed& operator<(const Fixed& copy);  
+        Fixed& operator>=(const Fixed& copy);  
+        Fixed& operator<=(const Fixed& copy);  
+        Fixed& operator==(const Fixed& copy);  
+        Fixed& operator!=(const Fixed& copy);  
+        Fixed& operator+(const Fixed& copy);  
+        Fixed& operator-(const Fixed& copy);  
+        Fixed& operator*(const Fixed& copy);  
+        Fixed& operator/(const Fixed& copy);  
+        Fixed& operator=(const Fixed& copy);  
+        Fixed& operator=(const Fixed& copy);  
+        Fixed& operator=(const Fixed& copy);  
+        Fixed& operator=(const Fixed& copy);  
+        Fixed& operator=(const Fixed& copy);  
+        Fixed& operator=(const Fixed& copy);  
+        Fixed& operator=(const Fixed& copy);  
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+
+#endif
