a7dfc6fd3cdcc4db25157d0f30579d3a6d3ce57e
Mustafa Emre Yılmaz <mustafaemreyilmz@gmail.com>
2026-07-07 17:16:58 +0300

    ex01 eklendi

--- Cpp08/ex01/Span.cpp
+++ Cpp08/ex01/Span.cpp
@@ -2,10 +2,21 @@
 
 Span::Span()
 { 
+    _N = 0;
+}
+
+Span::Span(unsigned int n)
+{
+    _N = n;
 }
 
 Span::Span(const Span& other)
 {
+    if (this != &other)
+    {
+        this->_N = other._N;
+        this->_array = other._array;
+    }
 }
 
 Span::~Span()
@@ -14,4 +25,47 @@ Span::~Span()
 
 Span& Span::operator=(const Span& other)
 {
+    if (this != &other)
+    {
+        this->_N = other._N;
+        this->_array = other._array;
+    }
+    return *this;
+    
+}
+
+void Span::addNumber(int number) {
+    if (_array.size() >= _N) 
+        throw std::out_of_range("Index out of bounds!");
+    _array.push_back(number);
+}
+
+int Span::shortestSpan()
+{
+    if (_array.size() < 2)
+        throw std::out_of_range("Index out of bounds!");
+
+    std::vector<int> copy = _array;
+    std::sort(copy.begin(), copy.end());
+
+    int min_span = copy[1] - copy[0];
+    for (size_t i = 1; i < copy.size() - 1; i++)
+    {
+        int diff = copy[i + 1] - copy[i];
+        if (diff < min_span)
+            min_span = diff;
+    }
+    
+    return min_span;
+}
+
+int Span::longestSpan()
+{
+    if (_array.size() < 2)
+        throw std::out_of_range("Index out of bounds!");
+
+    int min_val = *std::min_element(_array.begin(), _array.end());
+    int max_val = *std::max_element(_array.begin(), _array.end());
+
+    return max_val - min_val;
 }
--- Cpp08/ex01/Span.hpp
+++ Cpp08/ex01/Span.hpp
@@ -3,12 +3,14 @@
 
 #include <vector>
 #include <stdexcept>
+#include <algorithm>
+#include <iostream>
 
 class Span
 {
     private:
         unsigned int _N;
-        std::vector<int> array;
+        std::vector<int> _array;
     public:
         Span();
         ~Span();
@@ -21,7 +23,13 @@ class Span
         int longestSpan();
 
         template <typename T>
-        void addNumber(T first, T second);
+        void addNumber(T first, T second)
+        {
+            if (std::distance(first, second) + _array.size() > _N)
+                throw std::out_of_range("Kapasite yetersiz! Bu kadar eleman eklenemez.");
+            
+            _array.insert(_array.end(), first, second);
+        }
 };
 
 #endif
